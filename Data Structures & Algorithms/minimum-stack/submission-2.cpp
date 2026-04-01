11c1973196e929b27a533cee725e876688c105ae
Mã Kiều <tuan.ma866@hcmut.edu.vn>
2026-04-01 16:22:27 +0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+    stack<pair<int, int>> st;
+
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()) st.push({val, val});
+        else {
+            int minVal=std::min(val, st.top().second);
+            st.push({val, minVal});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
